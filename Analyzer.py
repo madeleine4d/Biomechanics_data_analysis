713e04cb2efcca188f013858e728d27863d2ef9c
Maddy <madeleiner4d@gmail.com>
2025-02-22 13:32:37 -0800

    clear command added

--- Analyzer.py
+++ Analyzer.py
@@ -1,8 +1,4 @@
-# TODO: 
-# add a ClearCell() function
-#
-
-#import required packages
+# import required packages
 import pandas as pd
 from colorama import Fore, Back, Style
 from pathlib import Path
@@ -10,16 +6,16 @@ import traceback as tb
 from scipy import stats
 import numpy as np
 
-#create main dataframe that will hold data for all participant. This will be replaced by an existing file is 'mount' command by user
+# create main dataframe that will hold data for all participant. This will be replaced by an existing file is 'mount' command by user
 DATA = pd.DataFrame(columns=['MVC max', 'DLS mean', 'SLS mean', 'DLS %', 'SLS %'], index=['Name'])
 
-#export DATA into a csv. User must include ".csv". can make folders if path specified does not exist
+# export DATA into a csv. User must include ".csv". can make folders if path specified does not exist
 def Export(data, path):
     path = Path(path)
     path.parent.mkdir(parents=True, exist_ok=True)
     data.to_csv(path)
 
-#returns a dataframe containing a xlsx, csv, or txt file. Takes file path and a bool indicating if the data already has correct formatting
+# returns a dataframe containing a xlsx, csv, or txt file. Takes file path and a bool indicating if the data already has correct formatting
 def FileMounter(path, YNformated=False):    
     match YNformated:
         case True:
@@ -37,28 +33,29 @@ def FileMounter(path, YNformated=False):
             else:
                 raise(ImportError(Fore.RED + "file type not supported. Please double check you typed the path correctly." + Style.RESET_ALL))    
 
-#Takes files output by PowerLab and formats them to be compatible with this analyzer
+# Takes files output by PowerLab and formats them to be compatible with this analyzer
 def DataFormater(dataframe):
        cutdata = dataframe.iloc[5: , :2]
        cutdata.columns = ['Time', 'mVs']
        print (cutdata)
        return cutdata
 
-#take a dataframe and a column name with in that data frame and returns the higest aboslute value in that column
+# take a dataframe and a column name with in that data frame and returns the higest aboslute value in that column
 def FindGreatestValue(dataframe, columnName):
     return max([abs(num) for num in dataframe[columnName].tolist()])
 
-#take a dataframe and a column name with in that data frame and returns the mean of the aboslute values in that column
+# take a dataframe and a column name with in that data frame and returns the mean of the aboslute values in that column
 def FindAbsMean(dataframe, columnName):
     absolutes = [abs(num) for num in dataframe[columnName].tolist()]
     return sum(absolutes) / len(absolutes)
 
-#take a subcommand (tack) that specifies if a DLS or SLS is to be normalized and add the normalized data to DATA
+# take a subcommand (tack) that specifies if a DLS or SLS is to be normalized and add the normalized data to DATA
 def Normalize(tack):
     for i in (i for i in DATA.index if pd.isnull(DATA.loc[i, tack + ' %'])):
         DATA.loc[i, tack + ' %'] = DATA.loc[i, tack + ' mean'] / DATA.loc[i, 'MVC max']
-        
-def TTest(array1: list, array2:list, arrayname1: str, arrayname2: str):
+  
+# run a standard 2 way t-test given 2 list of data and the names of the lists. Prints t-test to a text file   
+def TTest(array1: list, array2: list, arrayname1: str, arrayname2: str):
     tTest = stats.ttest_ind(array1, array2)
     print (tTest)
     tTestString = 'statistic: ' + str(getattr(tTest, 'statistic')) + '\np-value: ' + str(getattr(tTest, 'pvalue')) + '\ndegrees of freedom: ' + str(getattr(tTest, 'df'))
@@ -66,7 +63,7 @@ def TTest(array1: list, array2:list, arrayname1: str, arrayname2: str):
         text_file.write(tTestString)
 
 
-#Fills a cell at a particular index (participant name) and column with a given value. Also takes a bool to allow the function to overwrite existing cell value
+# Fills a cell at a particular index (participant name) and column with a given value. Also takes a bool to allow the function to overwrite existing cell value
 def AddEntry(participant, valueName, value, YNrewrite):
     global DATA
     try:
@@ -83,7 +80,7 @@ def AddEntry(participant, valueName, value, YNrewrite):
     if (YNrewrite):
             DATA.loc[participant, valueName] = value
 
-
+# coordinates new data entery
 def AddRun(currentCommand, path, participant): # TODO clean this up
     match currentCommand:
         case 'MVC' | 'MVC -R'| 'DLS' | 'DLS -R' | 'SLS' | 'SLS -R':
@@ -129,15 +126,31 @@ def AddRun(currentCommand, path, participant): # TODO clean this up
                     case 'SLS -M':
                         AddEntry(subnames[i], 'SLS mean', FindAbsMean(tempData, 'mVs'), False)
 
+# removes a cell or a row depening on if it is passed a string or a list
+def Clear(pos):
+    global DATA
+    match pos:
+        case list():
+            DATA.loc[pos[0], pos[1]] = np.NaN
+            print('Data at ' + pos[0] + ', ' + pos[1] + 'was removed')
+        case str():
+            DATA = DATA.drop(pos, axis=0)
+            print('column ' + pos + ' was removed')
 
-#Prints a list of commands
+
+
+
+# Prints a list of commands
 def PrintHelp():
     print('''What would you like to do? 
           Here is a list of commands: 
           'help' (print this list again)
           'mount' (if you have a master data file you have been using, this comands allows you to import it)
           'MVC' or 'DLS' or 'SLS' (import and add MVC/DLS/SLS data. Add -R to allow this command to rewrite cells; add an -M to import multiple files of the same type at once)
+          'addSubj' (add MVC, DLS, and SLS and run all normalization for a new participant)
           'normalize' (normalizes DLS or SLS means to MVC max. Use -DLS or -SLS to indicate what needs to be normalized)
+          't-test' (run a standard t-test between two columns of data. outputs to a .txt file)
+          'clear' (remove a cell (w/ -C) or an entire participant (w/ -P))
           'show' (print the entire mounted data. add -S to only show a several line summery)
           'export'
           'quit' (stop the program)
@@ -176,14 +189,11 @@ while loop:
             case 'clear -C' | 'clear -P':
                 subcommand = currentCommand.split('-')[-1]
                 match subcommand:
-                    case'C': 
-                        pos = input('enter the the participant name and column of the cell you want to erase separated by a comma:\n').split(', ')
-                        DATA.loc[pos[0], pos[1]] = np.NaN
-                        print(DATA.loc[pos[0], pos[1]] + 'was removed')
+                    case'C':
+                        Clear(input('enter the the participant name and column of the cell you want to erase separated by a comma:\n').split(', '))
                     case 'P': 
-                        pos = input('enter the participants name you want to remove:\n')
-                        DATA = DATA.drop(pos, axis=0)
-                        print('column' + pos + 'was removed')
+                        Clear(input('enter the participants name you want to remove:\n'))
+
                         
             case 'export':
                 Export(DATA, input('Please enter the path and file name you would like: \n'))
